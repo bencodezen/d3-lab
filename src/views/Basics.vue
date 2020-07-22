03583da059d0d5628b16edad382c64ca12f712bd
Ben Hong <ben@bencodezen.io>
2020-07-21 23:27:38 -0400

    feature: add headings and formatting

--- src/views/Basics.vue
+++ src/views/Basics.vue
@@ -11,11 +11,19 @@ export default {
 </script>
 
 <template>
-  <main>
+  <main class="wrapper">
     <h1>Section 2: Getting Started with D3</h1>
+    <h2>Practicing Basic D3 Functions</h2>
     <AddSvgExercise />
+    <h2>Generate Rectangles Based on JSON</h2>
     <BuildingVisualization />
   </main>
 </template>
 
-<style></style>
+<style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
